8c031754d8dcce5d4e44700e85948f360a6a46e3
Omarmoatz <omar.raouf.002@gmail.com>
2024-03-20 22:30:24 +0200

    design models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.functions import Now
 
-# Create your models here.
+class Todo(models.Model):
+    task = models.CharField( max_length=50,db_defaulf='default_task')
+    created_at = models.DateTimeField( db_defaulf=Now(utc=True))
+    completed_at = models.DateTimeField(null=True)
+    completed = models.BooleanField( db_defaulf=False)
+    
+    def complete(self):
+        self.completed_at = Now()
+        self.save()
+        
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.task
