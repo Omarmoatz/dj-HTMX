2bb7e441e095239598d0a3b78101e62cd90002dc
Omarmoatz <omar.raouf.002@gmail.com>
2024-03-22 15:26:28 +0200

    add requirements file

--- todo/models.py
+++ todo/models.py
@@ -2,14 +2,9 @@ from django.db import models
 from django.db.models.functions import Now
 
 class Todo(models.Model):
-    task = models.CharField( max_length=50,db_defaulf='default_task')
-    created_at = models.DateTimeField( db_defaulf=Now(utc=True))
-    completed_at = models.DateTimeField(null=True)
-    completed = models.BooleanField( db_defaulf=False)
-    
-    def complete(self):
-        self.completed_at = Now()
-        self.save()
+    task = models.CharField( max_length=50,db_default='default_task')
+    created_at = models.DateTimeField( db_default=Now(utc=True))
+    completed = models.BooleanField( db_default=False)
         
     class Meta:
         ordering = ['created_at']
