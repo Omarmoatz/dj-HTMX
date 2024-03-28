9044761d0fb858fa71425bb5b15631b28c9fe1f8
Omarmoatz <omar.raouf.002@gmail.com>
2024-03-28 18:11:28 +0200

    add todo template

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.db.models.functions import Now
+from  django.contrib.auth.models import User
 
 class Todo(models.Model):
+    user = models.ForeignKey(User, related_name='user_task', on_delete=models.CASCADE)
     task = models.CharField( max_length=50,db_default='default_task')
     created_at = models.DateTimeField( db_default=Now(utc=True))
     completed = models.BooleanField( db_default=False)
